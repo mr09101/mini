8dd490dfc43a81106e51a7c89bb6a5102bc9bb2b
kim <ilos1212@naver.com>
2018-08-10 16:54:14 +0900

    최종뷰

--- mini/src/mini/Smanager.java
+++ mini/src/mini/Smanager.java
@@ -8,6 +8,9 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import java.awt.Font;
+import javax.swing.table.DefaultTableModel;
 
 public class Smanager {
 
@@ -48,35 +51,27 @@ public class Smanager {
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(12, 10, 760, 130);
+		panel.setBounds(12, 0, 760, 98);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("\uD68C\uC6D0 ID");
-		lblNewLabel.setBounds(140, 53, 57, 15);
+		lblNewLabel.setFont(new Font("±¼¸²", Font.PLAIN, 16));
+		lblNewLabel.setBounds(57, 49, 78, 22);
 		panel.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(206, 50, 116, 21);
+		textField.setBounds(131, 46, 180, 29);
 		panel.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnNewButton = new JButton("\uAC80\uC0C9");
-		btnNewButton.setBounds(455, 24, 109, 37);
+		btnNewButton.setBounds(353, 43, 109, 37);
 		panel.add(btnNewButton);
 		
 		JButton btnWjscprjator = new JButton("\uC804\uCCB4 \uAC80\uC0C9");
-		btnWjscprjator.setBounds(455, 71, 109, 37);
+		btnWjscprjator.setBounds(565, 29, 127, 64);
 		panel.add(btnWjscprjator);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(12, 171, 760, 248);
-		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(null);
-		
-		table = new JTable();
-		table.setBounds(0, 10, 760, 228);
-		panel_1.add(table);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBounds(12, 442, 760, 109);
@@ -84,15 +79,63 @@ public class Smanager {
 		panel_2.setLayout(null);
 		
 		JButton btnNewButton_2 = new JButton("\uCD94\uAC00");
-		btnNewButton_2.setBounds(52, 21, 133, 60);
+		btnNewButton_2.setBounds(59, 21, 133, 60);
 		panel_2.add(btnNewButton_2);
 		
 		JButton button = new JButton("\uC218\uC815");
-		button.setBounds(298, 21, 133, 60);
+		button.setBounds(256, 21, 133, 60);
 		panel_2.add(button);
 		
 		JButton button_1 = new JButton("\uC0AD\uC81C");
-		button_1.setBounds(530, 21, 133, 60);
+		button_1.setBounds(453, 21, 133, 60);
 		panel_2.add(button_1);
+		
+		JButton button_2 = new JButton("\uB2EB\uAE30");
+		button_2.setBounds(637, 58, 111, 41);
+		panel_2.add(button_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 102, 760, 351);
+		frame.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"\uD68C\uC6D0ID", "\uD68C\uC6D0 \uC774\uB984", "\uC0AC\uC6A9 \uC2DC\uAC04", "\uB0A8\uC740 \uC2DC\uAC04"
+			}
+		));
+		scrollPane.setViewportView(table);
 	}
 }
